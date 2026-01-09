b48fb674be4d7dff85024f0185217e0bc3446e29
YoungRheeCho <nrcho0331@gmail.com>
2026-01-09 17:31:20 +0900

    [Skrrrrrrrrrrrrrrrrrrr] unic backend first commit

---
+++ backend/unic/unic.go
@@ -0,0 +1,111 @@
+package unic
+
+import (
+	"context"
+	"io"
+	"time"
+
+	"github.com/rclone/rclone/fs"
+	"github.com/rclone/rclone/fs/config/configmap"
+	"github.com/rclone/rclone/fs/hash"
+)
+
+// Register with Fs
+func init() {
+	fs.Register(&fs.RegInfo{
+		Name:        "unic",
+		Description: "Unified Namespace of Integrated Cloudstorage",
+		NewFs:       NewFs,
+		MetadataInfo: &fs.MetadataInfo{
+			Help: `Any metadata supported by the underlying remote is read and written.`,
+		},
+		Options: []fs.Option{{
+			Name:     "upstreams",
+			Help:     "List of space separated upstreams.\n\nCan be 'upstreama:test/dir upstreamb:', '\"upstreama:test/space:ro dir\" upstreamb:', etc.",
+			Required: true,
+		}, {
+			Name:    "cache_time",
+			Help:    "Cache time of usage and free space (in seconds).\n\nThis option is only useful when a path preserving policy is used.",
+			Default: 120,
+		}},
+	})
+}
+
+type Fs struct {
+	name     string       // name of this remote
+	features *fs.Features // optional features
+	opt      Options      // parsed options
+	root     string       // the path we are working on
+}
+
+type Options struct {
+}
+
+type Object struct {
+}
+
+func NewFs(ctx context.Context, name, root string, m configmap.Mapper) (fs.Fs, error) {
+	f := &Fs{
+		name:     name,
+		root:     root,
+		features: &fs.Features{},
+	}
+	return f, nil
+}
+
+/* Fs */
+// Fs
+func (f *Fs) List(ctx context.Context, dir string) (fs.DirEntries, error) {
+	return nil, nil
+}
+
+func (f *Fs) NewObject(ctx context.Context, remote string) (fs.Object, error) {
+	return &Object{}, nil
+}
+
+func (f *Fs) Put(ctx context.Context, in io.Reader, src fs.ObjectInfo, options ...fs.OpenOption) (fs.Object, error) {
+	return &Object{}, nil
+}
+
+func (f *Fs) Mkdir(ctx context.Context, dir string) error { return nil }
+func (f *Fs) Rmdir(ctx context.Context, dir string) error { return nil }
+
+// Info
+// func (f *Fs) Name() string
+
+// func (f *Fs) Root() string
+
+// func (f *Fs) String() string
+
+//func (f *Fs) Features() *fs.Features
+
+func (f *Fs) Name() string           { return f.name }
+func (f *Fs) Root() string           { return f.root }
+func (f *Fs) String() string         { return f.name }
+func (f *Fs) Features() *fs.Features { return f.features }
+
+func (f *Fs) Precision() time.Duration {
+	return time.Second // 최소 1초 정밀도
+}
+
+func (f *Fs) Hashes() hash.Set {
+	return hash.Set(hash.None) // 최소 해시 없음
+}
+
+/* Object */
+// DirEntry
+func (o *Object) Fs() fs.Info                                            { return nil }
+func (o *Object) String() string                                         { return "object" }
+func (o *Object) Remote() string                                         { return "" }
+func (o *Object) ModTime(ctx context.Context) time.Time                  { return time.Now() }
+func (o *Object) Size() int64                                            { return 0 }
+func (o *Object) Hash(ctx context.Context, ty hash.Type) (string, error) { return "", nil }
+func (o *Object) Storable() bool                                         { return true }
+func (o *Object) SetModTime(ctx context.Context, t time.Time) error      { return nil }
+func (o *Object) Open(ctx context.Context, options ...fs.OpenOption) (io.ReadCloser, error) {
+	return io.NopCloser(nil), nil
+}
+func (o *Object) Update(ctx context.Context, in io.Reader, src fs.ObjectInfo, options ...fs.OpenOption) error {
+	return nil
+}
+func (o *Object) Remove(ctx context.Context) error { return nil }
