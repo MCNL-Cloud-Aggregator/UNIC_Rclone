b1a95511119082235035ee9e4676d3e149899c8c
shadis8372 <hs1261101@gmail.com>
2026-03-16 15:11:08 +0900

    [feat] unmount, mount 했을 때 cache 초기화2

--- cmd/cmount/mount.go
+++ cmd/cmount/mount.go
@@ -12,6 +12,7 @@ import (
 	"runtime"
 	"time"
 
+	"github.com/rclone/rclone/backend/unic"
 	"github.com/rclone/rclone/cmd/mountlib"
 	"github.com/rclone/rclone/fs"
 	"github.com/rclone/rclone/lib/atexit"
@@ -164,8 +165,8 @@ func mount(VFS *vfs.VFS, mountPath string, opt *mountlib.Options) (<-chan error,
 	// unmount
 	unmount := func() error {
 		// Fs가 unic일 경우 unic backend cache 초기화
-		if fsys.name == "unic" {
-			err := fsys.ClearOSDefaultDownloadDir()
+		if unicFs, ok := f.(*unic.Fs); ok {
+			err := unicFs.ClearOSDefaultDownloadDir()
 			if err != nil {
 				return err
 			}
